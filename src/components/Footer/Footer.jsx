1b8854ad4a5210735d3ee5f47a5c709c5257efca
Dineshdk1705 <dnsh555g@gmail.com>
2024-07-07 12:03:22 +0530

    ui fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Footer.scss";
 import bg from "../../images/footer-bg.jpg";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   const year = new Date().getFullYear();
@@ -9,29 +10,31 @@ const Footer = () => {
       <div className="footer__content container">
         <div className="footer__content__logo">
           <div className="logo">
-            <a href="/">Movie Palace</a>
+            <Link to="/">Movie Palace</Link>
           </div>
         </div>
         <div className="footer__content__menus">
           <div className="footer__content__menu">
-            <a href="/">Home</a>
-            <a href="/">Contact us</a>
-            <a href="/">Term of services</a>
-            <a href="/">About us</a>
+            <Link to="/">Home</Link>
+            <Link to="/">Contact us</Link>
+            <Link to="/">Term of services</Link>
+            <Link to="/">About us</Link>
           </div>
           <div className="footer__content__menu">
-            <a href="/">Live</a>
-            <a href="/">FAQ</a>
-            <a href="/">Premium</a>
-            <a href="/">Pravacy policy</a>
+            <Link to="/">Live</Link>
+            <Link to="/">FAQ</Link>
+            <Link to="/">Premium</Link>
+            <Link to="/">Pravacy policy</Link>
           </div>
           <div className="footer__content__menu">
-            <a href="/">You must watch</a>
-            <a href="/">Recent release</a>
-            <a href="/">Top IMDB</a>
+            <Link to="/">You must watch</Link>
+            <Link to="/">Recent release</Link>
+            <Link to="/">Top IMDB</Link>
           </div>
         </div>
-        <div className="copyright">@{year} | Movie, Inc or its affiliates</div>
+        <div className="copyright">
+          @{year} | Movie Palace, Inc or its affiliates
+        </div>
       </div>
     </div>
   );
